37041112b93127f3555a01888dcbe42cfa616a61
Daniel Stenberg <daniel@haxx.se>
2026-04-22 00:52:16 +0200

    curl: named globs in output file name for upload glob references
    Use parts of text from the upload filename field when that uses globbing
    by giving it a name the same way we do it for URL globs. For example, if
    you upload three files to a HTTP URL and want to save the corresponding
    responses in separate files:
    
        curl -T 'file{<num>1,2,3}' https://upload.example/ -o 'response-#<num>'
    
    Verified by test 2014

--- src/tool_urlglob.c
+++ src/tool_urlglob.c
@@ -703,7 +703,8 @@ CURLcode glob_next_url(char **globbed, struct URLGlob *glob)
 #define MAX_OUTPUT_GLOB_LENGTH (1024 * 1024)
 
 CURLcode glob_match_url(char **output, const char *filename,
-                        struct URLGlob *glob, SANITIZEcode *sc)
+                        struct URLGlob *glob, struct URLGlob *glob2,
+                        SANITIZEcode *sc)
 {
   struct dynbuf dyn;
   const char *ifilename = filename;
@@ -742,6 +743,9 @@ CURLcode glob_match_url(char **output, const char *filename,
          !curlx_str_single(&filename, '>')) {
         /* find the correct glob entry */
         pat = glob_find_name(glob, &name);
+        if(!pat && glob2 && glob_inuse(glob2))
+          /* scan the second glob list if there is one */
+          pat = glob_find_name(glob2, &name);
         if(!pat) {
           /* when the name is given correctly, it needs to be an existing glob
              name, which makes this an error */
